6f03e33026f7c15a8eb57e64034a772858a6029a
Ilya Fedorchenko <ilya.fedorchenko@gmail.com>
2019-01-13 02:58:36 +0300

    Initial Commit

---
+++ SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Илья Федорченко on 13/01/2019.
+//  Copyright © 2019 Илья Федорченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
